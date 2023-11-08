e0ba9bc18dbfa02478ad3c8f5dfffbfefd52fd92
Gpette01 <gpette01@ucy.ac.cy>
2023-11-08 12:56:03 +0200

    Fixec csv files and bulked entry in sql

--- BackEnd/SQL_Create.sql
+++ BackEnd/SQL_Create.sql
@@ -2,14 +2,14 @@
 CREATE TABLE [dbo].CUSTOMER (
   Email VARCHAR(30) NOT NULL,
   CHECK (Email LIKE '%@%.com'),
-  Phone_Number INT NOT NULL,
+  Phone_Number VARCHAR(8) NOT NULL,
   CHECK (
     Phone_Number LIKE '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'
   ),
   First_Name VARCHAR(15) NOT NULL,
   Last_Name VARCHAR(15) NOT NULL,
   UserName VARCHAR(20) NOT NULL,
-  Password VARCHAR(10) NOT NULL,
+  Password VARCHAR(20) NOT NULL,
   Birth_Date DATE NOT NULL,
   Loyalty_Points INT NOT NULL,
   CHECK(Loyalty_Points >= 0),
@@ -22,7 +22,7 @@ CREATE TABLE [dbo].PRODUCT (
   PID INT NOT NULL,
   Price INT NOT NULL,
   CHECK(Price >= 0),
-  Description VARCHAR(50) NOT NULL,
+  Description VARCHAR(50) NOT NULL
   DEFAULT 'No item description',
   Quantity INT NOT NULL,
   CHECK(Quantity >= 0),
@@ -38,10 +38,10 @@ CREATE TABLE [dbo].PRODUCT (
       'Pod'
     )
   ),
-  Popularity FLOAT NOT NULL,
+  Popularity INT NOT NULL,
   CHECK(
-    Popularity BETWEEN 1.0
-    and 10.0
+    Popularity BETWEEN 1
+    and 10
   ),
   CONSTRAINT [PK_PRODUCT] PRIMARY KEY ([PID] ASC)
 );
@@ -97,10 +97,10 @@ BULK
 INSERT
   [dbo].PRODUCT
 FROM
-  '.\Product.csv' WITH (
+  'G:\My Drive\UCY\UNIVERSITY(5TH semester)\EPL343\Big fucking project\epl343.winter23.team9\BackEnd\Product.csv' WITH (
     FIELDTERMINATOR = ',',
     -- Change to '\t' if your fields are tab-separated
-    ROWTERMINATOR = '\r\n',
+    ROWTERMINATOR = '\n',
     -- Windows-style newline '\r\n' might be needed
     FIRSTROW = 2,
     TABLOCK
@@ -110,11 +110,12 @@ BULK
 INSERT
   [dbo].Customer
 FROM
-  '.\Customer.csv' WITH (
+  'G:\My Drive\UCY\UNIVERSITY(5TH semester)\EPL343\Big fucking project\epl343.winter23.team9\BackEnd\Customer.csv' WITH (
     FIELDTERMINATOR = ',',
     -- Change to '\t' if your fields are tab-separated
-    ROWTERMINATOR = '\r\n',
+    ROWTERMINATOR = '\n',
     -- Windows-style newline '\r\n' might be needed
     FIRSTROW = 2,
     TABLOCK
+
   );
