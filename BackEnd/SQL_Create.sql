2f4da097036a656696ed43bb56d20236c4add04c
mdemet01 <mdemet01@ucy.ac.cy>
2023-11-16 10:36:27 +0200

    Update SQL_Create.sql
    Fix the sql script that created the database after the new ER diagram

--- BackEnd/SQL_Create.sql
+++ BackEnd/SQL_Create.sql
@@ -1,16 +1,15 @@
 --create customer table
-USE DB
-CREATE TABLE [dbo].CUSTOMER (
+USE DB CREATE TABLE [dbo].CUSTOMER (
   Email VARCHAR(30) NOT NULL,
   CHECK (Email LIKE '%@%.com'),
   Phone_Number VARCHAR(8) NOT NULL,
   CHECK (
     Phone_Number LIKE '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'
   ),
-  First_Name VARCHAR(15) NOT NULL,
-  Last_Name VARCHAR(15) NOT NULL,
-  UserName VARCHAR(20) NOT NULL,
-  Password VARCHAR(20) NOT NULL,
+  First_Name VARCHAR(30) NOT NULL,
+  Last_Name VARCHAR(30) NOT NULL,
+  UserName VARCHAR(30) NOT NULL,
+  Passwd VARCHAR(20) NOT NULL,
   Birth_Date DATE NOT NULL,
   Loyalty_Points INT NOT NULL,
   CHECK(Loyalty_Points >= 0),
@@ -20,12 +19,11 @@ CREATE TABLE [dbo].CUSTOMER (
 --create product table
 CREATE TABLE [dbo].PRODUCT (
   Product_Name VARCHAR(30) NOT NULL,
-  PID INT NOT NULL,
+  Product_ID INT NOT NULL,
   Price INT NOT NULL,
   CHECK(Price >= 0),
-  Description VARCHAR(50) NOT NULL
-  DEFAULT 'No item description',
-  Quantity INT NOT NULL,
+  Description VARCHAR(50) NOT NULL DEFAULT 'No item description',
+  Stock INT NOT NULL,
   CHECK(Quantity >= 0),
   Category VARCHAR(30) NOT NULL,
   CHECK (
@@ -44,6 +42,7 @@ CREATE TABLE [dbo].PRODUCT (
     Popularity BETWEEN 1
     and 10
   ),
+  Image_path VARCHAR(200) NOT NULL,
   CONSTRAINT [PK_PRODUCT] PRIMARY KEY ([PID] ASC)
 );
 
@@ -51,31 +50,56 @@ CREATE TABLE [dbo].PRODUCT (
 CREATE TABLE [dbo].ORDERS (
   OrderID INT NOT NULL,
   Status VARCHAR(15) NOT NULL,
+  CHECK (
+    Status IN (
+      'Pending',
+      'Ready for pickup',
+      'Completed',
+      'Cancelled',
+      'In progress',
+    )
+  ),
   Date DATE NOT NULL,
-  ID INT NOT NULL,
-  UserName VARCHAR(20) NOT NULL,
+  --FK
+  UserName VARCHAR(30) NOT NULL,
   CONSTRAINT [PK_ORDERS] PRIMARY KEY ([OrderID] ASC)
 );
 
 --create cart table
 CREATE TABLE [dbo].CART (
   Total_Amount INT NOT NULL,
-  PID INT NOT NULL,
-  Quantity INT NOT NULL,
-  UserName VARCHAR(20) NOT NULL,
+  Cart_ID INT NOT NULL,
+  --FK
+  UserName VARCHAR(30) NOT NULL,
+  CONSTRAINT [PK_CART] PRIMARY KEY ([Cart_ID] ASC)
 );
 
+--Many to many tables
+
+--create cart_product table
+CREATE TABLE [dbo].CART_PRODUCT (
+  Cart_Quantity INT NOT NULL,
+  --FK
+  Cart_ID INT NOT NULL,
+  Product_ID INT NOT NULL,
+  CONSTRAINT [PK_CART_PRODUCT] PRIMARY KEY ([Cart_ID],[Product_ID] ASC)
+);
+
+--create order_product table
+CREATE TABLE [dbo].ORDER_PRODUCT (
+  Order_Quantity INT NOT NULL,
+  --FK
+  Order_ID INT NOT NULL,
+  Product_ID INT NOT NULL,
+  CONSTRAINT [PK_ORDER_PRODUCT] PRIMARY KEY ([Order_ID],[Product_ID] ASC)
+);
+
+--FOREIGN KEYS
 --add foreign key for orders(Customer UserName)
 ALTER TABLE
   [dbo].ORDERS WITH CHECK
 ADD
   CONSTRAINT [FK_ORDERS_CUSTOMER] FOREIGN KEY ([UserName]) REFERENCES [dbo].CUSTOMER ([UserName])
-GO
-  --add foreign key for cart(Product id)
-ALTER TABLE
-  [dbo].CART WITH CHECK
-ADD
-  CONSTRAINT [FK_CART_PRODUCTS] FOREIGN KEY ([PID]) REFERENCES [dbo].PRODUCT ([PID])
 GO
   --add foreign key for cart(Customer Username)
 ALTER TABLE
@@ -83,16 +107,31 @@ ALTER TABLE
 ADD
   CONSTRAINT [FK_CART_CUSTOMER] FOREIGN KEY ([UserName]) REFERENCES [dbo].CUSTOMER ([UserName])
 GO
-  --makes foreign key(UserName) of cart unique
+
+--add foreign key for cart_product
 ALTER TABLE
-  [dbo].CART WITH CHECK
+  [dbo].CART_PRODUCT WITH CHECK
 ADD
-  CONSTRAINT [UNQ_UserName] UNIQUE ([UserName])
+  CONSTRAINT [FK_CART_PRODUCT_CART] FOREIGN KEY ([Cart_ID]) REFERENCES [dbo].CART ([Cart_ID])
 GO
 ALTER TABLE
-  dbo.Customer
+  [dbo].CART_PRODUCT WITH CHECK
 ADD
-  CONSTRAINT check_Age CHECK (DATEDIFF(year, Birth_Date, GETDATE()) >= 18);
+  CONSTRAINT [FK_CART_PRODUCT_PRODUCT] FOREIGN KEY ([Product_ID]) REFERENCES [dbo].PRODUCT ([Product_ID])
+GO
+
+--add foreign key for order_product
+ALTER TABLE
+  [dbo].ORDER_PRODUCT WITH CHECK
+ADD
+  CONSTRAINT [FK_ORDER_PRODUCT_ORDER] FOREIGN KEY ([Order_ID]) REFERENCES [dbo].ORDERS ([Order_ID])
+GO
+  --makes foreign key(UserName) of cart unique
+ALTER TABLE
+  [dbo].ORDER_PRODUCT WITH CHECK
+ADD
+  CONSTRAINT [FK_ORDER_PRODUCT_PRODUCT] FOREIGN KEY ([Product_ID]) REFERENCES [dbo].PRODUCT ([Product_ID])
+GO
 
 BULK
 INSERT
@@ -118,5 +157,4 @@ FROM
     -- Windows-style newline '\r\n' might be needed
     FIRSTROW = 2,
     TABLOCK
-
   );
